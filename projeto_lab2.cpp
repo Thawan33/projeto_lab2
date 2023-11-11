47cde36714bc7a077851d96b8e798470dba28262
João Kleber <128982751+joaokleber10@users.noreply.github.com>
2023-11-11 14:53:24 -0300

    Update projeto_lab2.cpp

--- projeto_lab2.cpp
+++ projeto_lab2.cpp
@@ -51,39 +51,153 @@ void nozama(Nopilha*& carrinho,double preco,int quant,string nome){
     produto -> nome = nome;
     adcionar_produto(carrinho,produto);
 }
-
+// FUNÇÃO PARA A ESCOLHA DOS PRODUTOS
 void menu(Nopilha* carrinho){
     char opc,opc2;
     int quant;
     double preco;
     string nome;
     cout << "Selecione a categoria dos produtos que deseja comprar \n";
-    cout << "[1] Eletronicos \n";
+    cout << " [1] Eletronicos \n [2] Eletrodomésticos \n [3] Alimentos \n [4] vestuário \n [5] Livros \n";
     cin >> opc;
     switch(opc){
     case '1':
-        cout << "ofertas do dia!\n";
-        cout << "[1] Notebook R$2000 \n [2] Celular R$1200 \n [3] Videogame R$2200 \n [4] Televisao Smart R$2500 \n ";
+        cout << "\n ** ofertas do dia!\n";
+        cout << "[1] Notebook UltraBook 15 - R$2000 \n [2] Celular UIphone 26 - R$1200 \n [3] Videogame Station Play 5 - R$2200 \n [4] Televisao Mansung Smart 4K - R$2500 \n ";
         cin >> opc2;
         switch(opc2){
         case '1':
-            cout << "Digite a quantidade que deseja comprar: ";
+            cout << "Quantidade: ";
             cin >> quant;
-            nozama(carrinho,2000,quant,"notebook");
+            nozama(carrinho,2000,quant,"notebook UltraBook 15");
         case '2':
-            cout << "Digite a quantidade que deseja comprar: ";
+            cout << "Quantidade: ";
             cin >> quant;
-            nozama(carrinho,1200,quant,"celular");
+            nozama(carrinho,1200,quant,"celular UIphone 26");
         case '3':
-            cout << "Digite a quantidade que deseja comprar: ";
+            cout << "Quantidade: ";
             cin >> quant;
-            nozama(carrinho,2200,quant,"videogame");
+            nozama(carrinho,2200,quant,"videogame Station Play 5");
         case '4':
-            cout << "Digite a quantidade que deseja comprar: ";
+            cout << "Quantidade: ";
             cin >> quant;
-            nozama(carrinho,2500,quant,"televisao smart");
+            nozama(carrinho,2500,quant,"televisao Mansung Smart 4K");
+            break;
         }
+        default:
+            cout << "Opcao invalida";
         break;
+    case '2':
+        cout << "ofertas do dia!\n";
+        cout << " [1] Geladeira R$2000 \n [2] Fogao R$1200 \n [3] Microondas R$2200 \n [4] Maquina de lavar R$2500 \n ";
+        cin >> opc2;
+        switch(opc2){
+            case '1':
+                cout << "Quantidade: ";
+                cin >> quant;
+                nozama(carrinho,2000,quant,"geladeira");
+                break;
+            case '2':
+                cout << "Quantidade: ";
+                cin >> quant;
+                nozama(carrinho,1200,quant,"fogao");
+                break;
+            case '3':
+                cout << "Quantidade: ";
+                cin >> quant;
+                nozama(carrinho,2200,quant,"microondas");
+                break;
+            case '4':
+                cout << "Quantidade: ";
+                cin >> quant;
+                nozama(carrinho,2500,quant,"maquina de lavar");
+                break;
+            default: 
+                cout << "Opcao invalida";
+    case '3':
+        cout << "Ofertas do dia!\n";
+        cout << " [1] Arroz R$10 \n [2] Feijao R$5 \n [3] Macarrao R$3 \n [4] Carne R$20 \n ";
+        cin >> opc2;
+        switch(opc2){
+            case '1':
+                cout << "Quantidade: ";
+                cin >> quant;
+                nozama(carrinho,10,quant,"arroz");
+                break;
+            case '2':
+                cout << "Quantidade: ";
+                cin >> quant;
+                nozama(carrinho,5,quant,"feijao");
+                break;
+            case '3':
+                cout << "Quantidade: ";
+                cin >> quant;
+                nozama(carrinho,3,quant,"macarrao");
+                break;
+            case '4':
+                cout << "Quantidade: ";
+                cin >> quant;
+                nozama(carrinho,20,quant,"carne");
+                break;
+            default:
+                cout << "Opcao invalida";
+        }
+    case '4':
+        cout << "Ofertas do dia!\n";
+        cout << " [1] Camiseta R$100 \n [2] Calca R$50 \n [3] Casaco R$300 \n [4] Sapato R$250 \n ";
+        cin >> opc2;
+        switch(opc2){
+            case '1':
+                cout << "Quantidade: ";
+                cin >> quant;
+                nozama(carrinho,10,quant,"camiseta");
+                break;
+            case '2':
+                cout << "Quantidade: ";
+                cin >> quant;
+                nozama(carrinho,5,quant,"calca");
+                break;
+            case '3':
+                cout << "Quantidade: ";
+                cin >> quant;
+                nozama(carrinho,3,quant,"casaco");
+                break;
+            case '4':
+                cout << "Quantidade: ";
+                cin >> quant;
+                nozama(carrinho,20,quant,"sapato");
+                break;
+            default:
+                cout << "Opcao invalida";
+        }
+    case '5':
+        cout << "Ofertas do dia!\n";
+        cout << " [1] Para todos os garotos que já matei R$10 \n [2] As tranças do rei careca (VOL 1) - R$50 \n [3] A volta dos que nao foram - R$34 \n [4] Seneca: Como ser pai de menina R$20 \n ";
+        cin >> opc2;
+        switch(opc2){
+            case '1':
+                cout << "Quantidade: ";
+                cin >> quant;
+                nozama(carrinho,10,quant,"livro 1");
+                break;
+            case '2':
+                cout << "Quantidade: ";
+                cin >> quant;
+                nozama(carrinho,5,quant,"livro 2");
+                break;
+            case '3':
+                cout << "Quantidade: ";
+                cin >> quant;
+                nozama(carrinho,3,quant,"livro 3");
+                break;
+            case '4':
+                cout << "Quantidade: ";
+                cin >> quant;
+                nozama(carrinho,20,quant,"livro 4");
+                break;
+            default:
+                cout << "Opcao invalida";
+        }
     
     default:
         break;
@@ -92,6 +206,6 @@ void menu(Nopilha* carrinho){
 
 int main(){
     Nopilha* carrinho = criar_Nopilha();
-
+    menu(carrinho);
     return 0;
 }
