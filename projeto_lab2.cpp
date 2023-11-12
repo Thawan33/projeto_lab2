a48a1695eaca2696bc423b7fb48a3a620ca012ff
João Kleber <128982751+joaokleber10@users.noreply.github.com>
2023-11-12 19:24:46 -0300

    Update projeto_lab2.cpp

--- projeto_lab2.cpp
+++ projeto_lab2.cpp
@@ -11,6 +11,58 @@ struct Nopilha{
     Item* produto;
     Nopilha* prox;
 };
+//implementação do historico de compras--------------------------------
+struct Nolista{
+    double valor_compra;
+    int quantidade_itens;
+    Nolista* prox;
+    Nolista* ant;
+};
+
+struct Descritor{
+    int tamanho;
+    Nolista* ini;
+    Nolista* fim;
+};
+
+Descritor* criar_lista(){
+    Descritor* d = new Descritor;
+    d->tamanho = 0;
+    d->ini = NULL;
+    d->fim = NULL;
+    return d;
+}
+
+void print(Nolista* lista){
+    if(lista != NULL){
+        cout << "quantidade de itens da compra: " << lista->quantidade_itens << " total da compra: " << lista->valor_compra << endl;
+        print(lista->prox);
+    }
+}
+
+void append(Descritor* lista,double valor,int quant){
+    Nolista* novo = new Nolista;
+    novo->valor_compra = valor;
+    novo->quantidade_itens = quant;
+    novo->prox = NULL;
+    novo->ant = NULL;
+    if(lista->ini == NULL){
+        lista->ini = novo;
+        lista->fim = novo;
+        lista->tamanho++;
+    }else{
+        Nolista* p = lista->ini;
+        while(p->prox != NULL){
+            p = p->prox;
+        }
+        p->prox = novo;
+        novo->ant = p;
+        lista->tamanho++;
+        lista->fim = novo;
+    }
+}
+
+//fim das implementações do historico de compras
 
 //função para iniciar a pilha apontada para NULL
 Nopilha* criar_Nopilha(){
@@ -63,14 +115,21 @@ if(carrinho == NULL){
 }   pagamento:
     system("cls");
     cout << " -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n RESUMO DA COMPRA \n -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n";
+    cout << "Produtos: \n";
+    while(carrinho != NULL){
+        Nopilha* aux = remover_produto(carrinho);
+        cout << aux->produto->nome << " - R$" << aux->produto->preco << " - " << aux->produto->quant << " unidade(s)\n";
+    }
+    cout << " -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n";
     cout << "O valor total da compra foi de: R$" << soma << endl;
-    cout << "Forma de pagamento: \n [1] Cartao de credito \n [2] Cartao de debito \n [3] Boleto \n [4] Pix \n -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n > ";
+    cout << " -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n";
+    cout << " Forma de pagamento: \n [1] Cartao de credito \n [2] Cartao de debito \n [3] Boleto \n [4] Pix \n -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n > ";
     char opc;
     cin >> opc;
     switch(opc){
         case '1':
-            cout << "Voce escolheu cartao de credito\n";
-            cout << "Deseja dividir em quantas parcelas? \n > ";
+            cout << "CARTAO DE CREDITO\n";
+            cout << "Deseja dividir em quantas parcelas? \n> ";
             int parcelas;
             cin >> parcelas;
             if (parcelas > 12 || parcelas < 1){
@@ -78,17 +137,17 @@ if(carrinho == NULL){
                 system ("pause");
                 goto pagamento;
             }
-            cout << "O valor das parcelas sera de: R$" << soma/parcelas << endl;
+            cout << "\nO valor das parcelas sera de: R$" << soma/parcelas << endl;
             break;
         case '2':
-            cout << "Voce escolheu cartao de debito\n em breve voce recebera um email com o comprovante de pagamento\n";
+            cout << "CARTAO DE DEBITO\nEm breve voce recebera um email com o comprovante de pagamento\n";
             break;
         case '3':
-            cout << "Voce escolheu boleto\n";
-            cout << "O boleto foi enviado para o seu email\n Voce tem 3 dias para pagar o boleto :)\n";
+            cout << "BOLETO\n";
+            cout << "O boleto foi enviado para o seu email\nVoce tem 3 dias até o vencimento :)\n";
             break;
         case '4':
-            cout << "Voce escolheu pix\n o codigo do pix foi enviado para o seu email\n Voce tem 30 minutos para pagar o pix :)\n";
+            cout << "PIX\nO codigo do pix foi enviado para o seu email\nVoce tem 30 minutos para pagar o pix :)\n";
             break;
         default:
             cout << "Opcao invalida";
@@ -96,7 +155,7 @@ if(carrinho == NULL){
             goto pagamento;
     }
 
-
+    cout << " -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n";
     cout << "Obrigado por comprar na Nozama! Volte sempre :D";
     
 }
@@ -104,6 +163,7 @@ if(carrinho == NULL){
 // FUNÇÃO PARA A ESCOLHA DOS PRODUTOS
 void menu(Nopilha* carrinho){
     char opc,opc2;
+    string tamanho;
     int quant;
     double preco;
     string nome;
@@ -111,9 +171,12 @@ void menu(Nopilha* carrinho){
     system("cls");
     cout << "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\nSEJA BEM-VINDO A NOZAMA! \n-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n";
     cout << "CATEGORIA DE PRODUTOS DISPONIVEIS \n-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n";
-    cout << " [1] Eletronicos \n [2] Eletrodomesticos \n [3] Alimentos \n [4] vestuario \n [5] Livros \n -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n [6] Finalizar compra \n -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n > ";
+    cout << " [1] Eletronicos \n [2] Eletrodomesticos \n [3] Alimentos \n [4] vestuario \n [5] Livros \n -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n [6] Carrinho de compras \n [7] Finalizar compra \n [0] Fechar Aplicativo -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n > ";
     cin >> opc;
     switch(opc){
+     case '0':
+        cout << "\nVolte sempre :D";
+        cout <<"\n-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n";
     case '1':
         cout << "\n *** OFERTAS DO DIA! ***\n -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n";
         cout << " [1] Notebook UltraBook 15 - R$2000 \n [2] Celular UIphone 26 - R$1200 \n [3] Videogame Station Play 5 - R$2200 \n [4] Televisao Mansung Smart 4K - R$2500 \n-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n > ";
@@ -162,7 +225,7 @@ void menu(Nopilha* carrinho){
         
 }
     case '2':
-        cout << "ofertas do dia!\n";
+        cout << "\n *** OFERTAS DO DIA! ***\n -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n";
         cout << " [1] Geladeira R$2000 \n [2] Fogao R$1200 \n [3] Microondas R$2200 \n [4] Maquina de lavar R$2500 \n-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n >  ";
         cin >> opc2;
         switch(opc2){
@@ -207,7 +270,7 @@ void menu(Nopilha* carrinho){
 					};
         }
     case '3':
-        cout << "Ofertas do dia!\n -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n";
+        cout << "\n *** OFERTAS DO DIA! ***\n -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n";
         cout << " [1] Arroz R$10 \n [2] Feijao R$5 \n [3] Macarrao R$3 \n [4] Carne R$20 \n-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n >  ";
         cin >> opc2;
         switch(opc2){
@@ -252,35 +315,42 @@ void menu(Nopilha* carrinho){
 					};
         }
     case '4':
-        cout << "Ofertas do dia!\n";
+        cout << "\n *** OFERTAS DO DIA! ***\n -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n";
         cout << " [1] Camiseta R$100 \n [2] Calca R$50 \n [3] Casaco R$300 \n [4] Sapato R$250 \n-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n >  ";
         cin >> opc2;
+        
         switch(opc2){
             case '1':
-                cout << "Quantidade: ";
+            	cout << "Tamanhos disponiveis: [P] [M] [GG]\n > ";
+            	cin >> tamanho;
+                cout << "\nQuantidade: ";
                 cin >> quant;
-                nozama(carrinho,10,quant,"camiseta");
+                nozama(carrinho,100,quant,"camiseta");
                 cout << "Produto adicionado com sucesso!\n";
                 system ("pause");
                 goto inicio;
             case '2':
+            	cout << "Tamanhos disponiveis: [37] [39] [42]\n > ";
+            	cin >> tamanho;
                 cout << "Quantidade: ";
                 cin >> quant;
-                nozama(carrinho,5,quant,"calca");
+                nozama(carrinho,50,quant,"calca");
                 cout << "Produto adicionado com sucesso!\n";
                 system ("pause");
                 goto inicio;
             case '3':
+                cout << "Tamanhos disponiveis: [P] [M] [G]\n > ";
+            	cin >> tamanho;
                 cout << "Quantidade: ";
                 cin >> quant;
-                nozama(carrinho,3,quant,"casaco");
+                nozama(carrinho,300,quant,"casaco");
                 cout << "Produto adicionado com sucesso!\n";
                 system ("pause");
                 goto inicio;
             case '4':
                 cout << "Quantidade: ";
                 cin >> quant;
-                nozama(carrinho,20,quant,"sapato");
+                nozama(carrinho,250,quant,"sapato");
                 cout << "Produto adicionado com sucesso!\n";
                 system ("pause");
                 goto inicio;
@@ -297,36 +367,36 @@ void menu(Nopilha* carrinho){
 					};
         }
     case '5':
-        cout << "Ofertas do dia!\n";
-        cout << " [1] Para todos os garotos que já matei R$10 \n [2] As tranças do rei careca (VOL 1) - R$50 \n [3] A volta dos que nao foram - R$34 \n [4] Seneca: Como ser pai de menina R$20 \n-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n >  ";
+        cout << "\n *** OFERTAS DO DIA! ***\n -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n";
+        cout << " [1] Para todos os garotos que já matei R$12,50 \n [2] As tranças do rei careca (VOL 1) - R$50 \n [3] A volta dos que nao foram - R$36,90 \n [4] Seneca: Como ser pai de menina R$19,90 \n-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n >  ";
         cin >> opc2;
         switch(opc2){
             case '1':
                 cout << "Quantidade: ";
                 cin >> quant;
-                nozama(carrinho,10,quant,"livro 1");
+                nozama(carrinho,12.50,quant,"livro 1");
                 cout << "Produto adicionado com sucesso!\n";
                 system ("pause");
                 goto inicio;
             case '2':
                 cout << "Quantidade: ";
                 cin >> quant;
-                nozama(carrinho,5,quant,"livro 2");
-                cout << "Produto adicionado com sucesso!";
+                nozama(carrinho,50,quant,"livro 2");
+                cout << "Produto adicionado com sucesso!\n";
                 system ("pause");
                 goto inicio;
             case '3':
                 cout << "Quantidade: ";
                 cin >> quant;
-                nozama(carrinho,3,quant,"livro 3");
-                cout << "Produto adicionado com sucesso!";
+                nozama(carrinho,36.90,quant,"livro 3");
+                cout << "Produto adicionado com sucesso!\n";
                 system ("pause");
                 goto inicio;
             case '4':
                 cout << "Quantidade: ";
                 cin >> quant;
-                nozama(carrinho,20,quant,"livro 4");
-                cout << "Produto adicionado com sucesso!";
+                nozama(carrinho,19.90,quant,"livro 4");
+                cout << "Produto adicionado com sucesso!\n";
                 system ("pause");
                 goto inicio;
             default:
@@ -341,7 +411,33 @@ void menu(Nopilha* carrinho){
 	                	goto inicio;
 					};
         }
-	case '6':
+    case '6':
+        system("cls");
+        char remove;
+        int cont = 0;
+        cout << " -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n CARRINHO DE COMPRAS \n -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n";
+        if(carrinho == NULL){
+        	cout << "Nenhum produto adicionado :( ";
+        } else{
+        while(carrinho != NULL){
+            Nopilha* aux = remover_produto(carrinho);
+            cont ++;
+            cout << "["<< cont << "] - "<< aux->produto->nome << " - R$" << aux->produto->preco << " - " << aux->produto->quant << " unidade(s)\n";
+        }
+        }
+        cout << " -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n";
+        cout << "Deseja cancelar o carrinho? [S] [N]\n >";
+            if(remove == 'S' || remove == 's'){
+             carrinho == NULL;
+            }
+            else{
+                goto inicio;          
+              }
+        cout << " -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n";
+
+    
+
+	case '7':
         //chamar função de finalizar compra
         finalizar_compra(carrinho);
         break;
@@ -351,6 +447,7 @@ void menu(Nopilha* carrinho){
 
 }
 }
+
 
 int main(){
     Nopilha* carrinho = criar_Nopilha();
